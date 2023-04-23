a5bf56b95d6d83b535a911f6e9b1b087f705d9f0
Yash7426 <lcs2022057@iiitl.ac.in>
2023-04-23 06:28:33 +0530

    Revert "bugs fixed"
    This reverts commit 4e070daaa2854107a0db7feae4c2cf0b397f15b2.

--- codemarkfrontend/src/components/Signup.js
+++ codemarkfrontend/src/components/Signup.js
@@ -20,23 +20,7 @@ export default function Signup() {
                 college_name: input3,
                 enrollment_number: input4,
                 joining_year: input5,
-                semester: input6
-            });
-            console.log(auth.currentUser.email, auth.currentUser.uid)
-        }
-    }
-    function writeStudentDataInUser(input1, input2, input3, input4, input5, input6) {
-        if (input1 == "" || input2 == "" || input3 == "" || input4 == "" || input5 == "" || input6 == "") {
-            console.log("please provide some input");
-        } else {
-            set(ref(db, 'users/' + auth.currentUser.uid), {
-                email: input1,
-                name: input2,
-                college_name: input3,
-                enrollment_number: input4,
-                joining_year: input5,
                 semester: input6,
-                user_type:"student"
             });
             console.log(auth.currentUser.email, auth.currentUser.uid)
         }
@@ -48,20 +32,7 @@ export default function Signup() {
             set(ref(db, 'teacher/' + auth.currentUser.uid), {
                 email: input1,
                 name: input2,
-                college_name: input3
-            });
-            console.log(auth.currentUser.email, auth.currentUser.uid)
-        }
-    }
-    function writeTeacherDatainUser(input1, input2, input3) {
-        if (input1 == "" || input2 == "" || input3 == "") {
-            console.log("please provide some input");
-        } else {
-            set(ref(db, 'users/' + auth.currentUser.uid), {
-                email: input1,
-                name: input2,
                 college_name: input3,
-                user_type:"teacher"
             });
             console.log(auth.currentUser.email, auth.currentUser.uid)
         }
@@ -121,7 +92,7 @@ export default function Signup() {
     }
     function RePassword() {
         return (
-            <label htmlFor="signup-cpassword">Confirm Password<input id="signup-cpassword" type="password" placeholder="Re-enter Password" name="signup-cpassword" /></label>
+            <label for="signup-cpassword">Confirm Password<input id="signup-cpassword" type="password" placeholder="Re-enter Password" name="signup-cpassword" /></label>
         );
     }
     function SignUpfnStudent(e) {
@@ -140,7 +111,6 @@ export default function Signup() {
                 const user = userCredential.user;
                 console.log(user)
                 writeStudentData(email, name, college_name, enrollment_number, joining_year, semester);
-                writeStudentDataInUser(email, name, college_name, enrollment_number, joining_year, semester);
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -157,7 +127,6 @@ export default function Signup() {
                 const user = userCredential.user;
                 console.log(user)
                 writeTeacherData(email, name, college_name);
-                writeTeacherDatainUser(email, name, college_name);
             })
             .catch((error) => {
                 const errorCode = error.code;
